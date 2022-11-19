0eb19a693e3d4ccb862c01af49db648495f07542
Joshua Doud <bigjj3393@yahoo.com>
2022-11-18 19:57:33 -0800

    Repaired drop down selection to eliminate default

--- index.js
+++ index.js
@@ -49,17 +49,14 @@ function addNinja(ninjaObj) {
 
 function renderOneNinja(ninja) {
     const ninjaCard = document.createElement("div")
-    ninjaCard.className = "ninja-card"
+    ninjaCard.classList = "ninja-card"
     ninjaCard.id = `${ninja.name}`
     ninjaCard.innerHTML =`
-        <img src="${ninja.imageUrl}">
-        <h4>Name</h4>
-        <p>${ninja.name}</p>
-        <h4>Village</h4>
-        <p>${ninja.village}</p>
-        <h4>Rank</h4>
-        <p>${ninja.rank}</p>
-        <p class="power" style="display:none;">${ninja.power}</p> 
+            <img src="${ninja.imageUrl}">
+            <p>Name:${ninja.name}</p>
+            <p>Village:${ninja.village}</p>
+            <p>Rank:${ninja.rank}</p>
+            <p class="power" style="display:none;">${ninja.power}</p> 
     `
     document.querySelector("#ninjaContainer").appendChild(ninjaCard)
 }
